3badac6e729133763c0e03116483bd1306394fe0
Sandi Cook <sandseas1@icloud.com>
2018-02-12 18:49:43 +0000

    did not work as expected, will revisit

--- index.php
+++ index.php
@@ -1,66 +1,107 @@
 <?php
+
+if ( ! empty ( $_POST ) ) {
+    // Post data
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    
+    // Check for valid email
+    if ( ! $email = filter_input( INPUT_POST, 'email', FILTER_VALIDATE_EMAIL ) ) {
+        die('Invalid email');
+    }
+
+//Database credentials
 $db_user = 'sandicook';
 $db_pass = '';
 $db_name = 'forms';
 $db_host = '127.0.0.1';
 $db_port = 3306;
 
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-if ($mysqli->connect_errno) {
+    // Fire up the connection
+    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
     
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-  
+    // Check we're connected
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+    
+    if (isset($_POST['submit'])) { // Note: I removed the exclamation mark before isset because it made no sense
+    $user_name = $_POST['name']; 
+    $user_email = $_POST['email']; 
+
+    if($user_name  && $user_email) {
+        $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+        $stmt = mysqli_stmt_init($link);
+        mysqli_stmt_prepare($stmt, "INSERT INTO user (`name`, `email`) VALUES (?,?)");
+        mysqli_stmt_bind_param($stmt, 'ss', $user_name, $user_email);
+        mysqli_stmt_execute($stmt);
+    }
 }
 
-$objects_table = 
-	"CREATE TABLE IF NOT EXISTS objects (
-		ID bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-		post_title text NOT NULL,
-		post_content longtext NOT NULL,
-		post_name varchar(20) NOT NULL,
-		post_date datetime NOT NULL,
-		PRIMARY KEY (ID)
-	)";
-
-if ($mysqli->query($objects_table) === TRUE) {
-    printf("Table objects successfully created.\n");
+    // Get ID of row we just inserted
+    $id = $mysqli->insert_id;
+    echo "working";
+    // Get the data we just submitted
+    $result = $mysqli->query("SELECT name, email FROM user WHERE ID = {$id}");
+    $user = $result->fetch_object();
 }
+   echo "working";
+   ?>
+ 
 
-$objects_meta_table = 
-	"CREATE TABLE IF NOT EXISTS object_meta (
-		meta_id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-		object_id bigint(20) UNSIGNED NOT NULL,
-		meta_key varchar (255),
-		meta_value longtext,
-		PRIMARY KEY (meta_id)
-	)";
 
-if ($mysqli->query($objects_meta_table) === TRUE) {
-    printf("<br />Table object_meta successfully created.\n");
-}
-
-$objects_tags_table = 
-	"CREATE TABLE IF NOT EXISTS object_tag_relationships (
-		ID bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-		object_id bigint(20) UNSIGNED NOT NULL,
-		tag_id bigint(20) UNSIGNED NOT NULL,
-		PRIMARY KEY (ID)
-	)";
-
-if ($mysqli->query($objects_tags_table) === TRUE) {
-    printf("<br />Table object_tag_relationships successfully created.\n");
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Simple Contact  Form</title>
+    <title>Form to Database</title>
+    
+    <style>
+            body {
+                margin: 0;
+                padding: 10px;
+                background:#208fe5;
+                font-family: Arial, sans-serif;
+                font-size: 14px;
+            }
+            .form {
+                background: #ffffff;
+                border-radius: 5px;
+                padding: 20px;
+                max-width: 480px;
+                margin: 15px auto;
+            }
+            .form, 
+            .text,
+            .email,
+            .submit {
+                display: block;
+                box-sizing: border-box;
+                width: 100%;
+            }
+            .text,
+            .email,
+            .submit {
+                padding: 10px;
+                font-size: 14px;
+                border: 1px solid #ccc;
+                border-radius: 3px;
+            }
+            .form h3 {
+                line-height: 1.2;
+                font-size: 14px;
+                margin-top: 5px;
+                font-weight: normal;
+            }
+            .submit {
+                border: none;
+                background: #FEBB13;
+                color: #222;
+                font-size: 24px;
+            }
+        </style>
 </head>
 <body>
 
@@ -69,26 +110,18 @@ if ($mysqli->query($objects_tags_table) === TRUE) {
 
 ?>
 
-<!--
-    <form action ="" method="post">
-        <div class = "form-field">
-            <input type = "text" class="text" name = "name" placeholder="Enter your name" required />
+        <form action="" method="post" class="form">
+            <?php if ( $user ) : ?>
+                <h3>Here's the data you submitted:</h3>
+                <p>Name: <?php echo htmlspecialchars($user->user_name, ENT_COMPAT, 'UTF-8'); ?></p>
+                <p>Email: <?php echo htmlspecialchars($user->user_email, ENT_COMPAT, 'UTF-8'); ?></p>
+            <?php endif; ?>
             
-        </div>
-    
-        <div class = "form-field">
-            <input type = "email" class="text" name = "email" placeholder="Enter your email" required />
-            
-        </div>
-    
-            <div class = "form-field">
-                
-                <button class = "button">Submit</button>
-            </div>
-            
-       
-    
-    </form>
--->
+            <h3>Enter your name and email address below:</h3>
+            <p><input type="text" name="name" class="text" placeholder="Enter your full name"></p>
+            <p><input type="email" name="email" class="text email" placeholder="Enter your email address"></p>
+            <p><input type="submit" class="submit" value="Submit"></p>
+        </form>
+        
 </body>
 </html>
